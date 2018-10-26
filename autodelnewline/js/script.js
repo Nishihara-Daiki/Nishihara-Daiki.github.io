0d008555e395d5fa9f3981e48436810ddc81a600
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-10-26 12:17:15 +0900

    [fix] 余計な文字削除

--- autodelnewline/js/script.js
+++ autodelnewline/js/script.js
@@ -8,7 +8,7 @@ $(function() {
 
 	$textarea.bind('input propertychange', function() {
 		s = $textarea.val().replace(/\n/g, ' ').replace(/ +/g, ' ').replace(/- /g, '');
-		s = s.replace(/Tab. /g, 'Table ').replace(/Fig. /g, 'Figure ').;
+		s = s.replace(/Tab. /g, 'Table ').replace(/Fig. /g, 'Figure ');
 		s = s.replace(/et al./g, 'et al');
 		$textarea.val(s);
 		$textarea.select();
