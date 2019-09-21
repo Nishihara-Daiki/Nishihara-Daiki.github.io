20740791a4144477d7933eb8026b1137135af66a
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2019-09-21 12:18:04 +0900

    [fix] e.g.で改行されないよう修正

--- autodelnewline/js/script.js
+++ autodelnewline/js/script.js
@@ -8,9 +8,9 @@ $(function() {
 
 	$textarea.bind('input propertychange', function() {
 		s = $textarea.val().replace(/\n/g, ' ').replace(/ +/g, ' ').replace(/- /g, '');
-		s = s.replace(/Tab. /g, 'Table ').replace(/Fig. /g, 'Figure ');
-		s = s.replace(/et al./g, 'et al');
+		s = s.replace(/Tab\. /g, 'Table ').replace(/Fig\. /g, 'Figure ');
 		s = s.replace(/\. /g, '.\n');
+		s = s.replace(/et al\.\n/g, 'et al. ').replace(/e\.g\.\n/g, 'e.g. ');
 		$textarea.val(s);
 		$textarea.select();
 		document.execCommand('copy');
